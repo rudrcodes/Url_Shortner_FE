205c7f023f43e6c10475db466fb85c3c731d24b3
Rudransh Aggarwal <rudrcodes@gmail.com>
2025-06-10 11:45:40 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import UrlShortner from "./Components/UrlShortner";
+
+function App() {
+  return (
+    <>
+      <h1>Url Shortner</h1>
+      <UrlShortner />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/UrlShortner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const UrlShortner = () => {
+  return <div>
+    UrlShortner
+    
+    </div>;
+};
+
+export default UrlShortner;
